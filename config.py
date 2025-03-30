4339c46229122a24424e381cfb599c8b25a8e511
enigmatic-bacon <bismuthalex@gmail.com>
2025-03-30 15:20:08 -0700

    Initial commit

---
+++ config.py
@@ -0,0 +1,16 @@
+import os
+from pathlib import Path
+
+# Path(__file__) wraps the current filepath in a path object
+# resolve() turns that path into an absolute one
+# parent gives the directory
+basedir = Path(__file__).resolve().parent
+
+
+class Config:
+    SECRET_KEY = os.getenv("SECRET_KEY", "you-will-never-guess")
+    # URI is a Uniform Resource Identifier -- it answers "What resource are you talking about"
+    # URL is a Uniform Resource Locator is a type of URI that also locates the resource
+    SQLALCHEMY_DATABASE_URI = (
+        os.getenv("DATABASE_URL", f"sqlite:///{basedir / "app.db"}")
+    )
