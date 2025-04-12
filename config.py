0f5bfdb379c873e5e1f0d69cd583609eefa39e60
enigmatic-bacon <bismuthalex@gmail.com>
2025-04-12 12:57:41 -0700

    Part 7: Error Handling

--- config.py
+++ config.py
@@ -14,3 +14,9 @@ class Config:
     SQLALCHEMY_DATABASE_URI = os.getenv(
         "DATABASE_URL", f"sqlite:///{basedir / 'app.db'}"
     )
+    MAIL_SERVER = os.getenv("MAIL_SERVER")
+    MAIL_PORT = os.getenv("MAIL_PORT", 25)
+    MAIL_USE_TLS = os.getenv("MAIL_USE_TLS") is not None
+    MAIL_USERNAME = os.getenv("MAIL_USERNAME")
+    MAIL_PASSWORD = os.getenv("MAIL_PASSWORD")
+    ADMINS = os.getenv("ADMINS")
