692e7f4716b4b790b058d5b6a68a230017e5c10d
enigmatic-bacon <bismuthalex@gmail.com>
2025-04-15 20:33:05 -0700

    Part 8: Followers

--- app/models.py
+++ app/models.py
@@ -11,12 +11,32 @@ from app import login
 from flask_login import UserMixin
 
 
+# Followers is a self relationsal table representing a many-to-many relationship
+# (i.e: each user can have many followers and can follow many users)
+# Since this table has no NEW data (only foreign keys) it need not be a model class
 # +-----------------------------+        +-----------------------------+
-# |           users            |        |           posts            |
+# |           users             |        |           followers         |
+# +-----------------------------+        +-----------------------------+
+# | id            INTEGER       |--+---> | follower_id       INTEGER   |
+# | username      VARCHAR(64)   |  |---> | followed_id       INTEGER   |
+# | email         VARCHAR(120)  |        +-----------------------------+
+# | password_hash VARCHAR(128)  |
+# +-----------------------------+
+followers = sa.Table(
+    "followers",
+    db.metadata,  # Where SQLAlchemy stores info about all the tables in the database
+    # Compound primary key means combination of these keys is unique (i.e: a user can only follow another one time)
+    sa.Column("follower_id", sa.Integer, sa.ForeignKey("user.id"), primary_key=True),
+    sa.Column("followed_id", sa.Integer, sa.ForeignKey("user.id"), primary_key=True),
+)
+
+
+# +-----------------------------+        +-----------------------------+
+# |           users             |        |           posts             |
 # +-----------------------------+        +-----------------------------+
 # | id            INTEGER       |<--     | id            INTEGER       |
-# | username      VARCHAR(64)   |  |     | body          VARCHAR(140) |
-# | email         VARCHAR(120)  |  |     | timestamp     DATETIME     |
+# | username      VARCHAR(64)   |  |     | body          VARCHAR(140)  |
+# | email         VARCHAR(120)  |  |     | timestamp     DATETIME      |
 # | password_hash VARCHAR(128)  |  |---> | user_id       INTEGER       |
 # +-----------------------------+        +-----------------------------+
 class User(UserMixin, db.Model):
@@ -29,6 +49,22 @@ class User(UserMixin, db.Model):
     about_me: so.Mapped[Optional[str]] = so.mapped_column(sa.String(140))
     last_seen: so.Mapped[Optional[datetime]] = so.mapped_column(
         default=lambda: datetime.now(timezone.utc)
+    )
+    following: so.WriteOnlyMapped["User"] = so.relationship(
+        secondary=followers,  # Configures the association table used for this relationship
+        primaryjoin=(
+            followers.c.follower_id == id
+        ),  # indicates the condition that links the user to the table (i.e: who is following)
+        secondaryjoin=(
+            followers.c.followed_id == id
+        ),  # indicates the condition that links the table to the user on the other side of the relationship (i.e: who is being followed)
+        back_populates="followers",  # When following is updated, followers are automatically updated as well
+    )
+    followers: so.WriteOnlyMapped["User"] = so.relationship(
+        secondary=followers,
+        primaryjoin=(followers.c.followed_id == id),  # .c is short for columns
+        secondaryjoin=(followers.c.follower_id == id),
+        back_populates="following",
     )
 
     def __repr__(self):
@@ -43,6 +79,65 @@ class User(UserMixin, db.Model):
 
     def check_password(self, password: str) -> bool:
         return check_password_hash(self.password_hash, password)
+
+    def follow(self, user):
+        if not self.is_following(user):
+            self.following.add(user)
+
+    def unfollow(self, user):
+        if self.is_following(user):
+            self.following.remove(user)
+
+    def is_following(self, user):
+        query = self.following.select().where(User.id == user.id)
+        return db.session.scalar(query) is not None
+
+    def followers_count(self):
+        # select would generally return the entities, but here we just care about their count
+        query = sa.select(sa.func.count()).select_from(
+            # Whenever a query is included as part of a larger query, SQLAlchemy requires the inner query to be converted to a sub-query
+            self.followers.select().subquery()
+        )
+        return db.session.scalar(query)
+
+    def following_count(self):
+        query = sa.select(sa.func.count()).select_from(
+            self.following.select().subquery()
+        )
+        return db.session.scalar(query)
+
+    def following_posts(self):
+        Author = so.aliased(User)
+        Follower = so.aliased(User)
+        # A join is a an operation that combines the rows from two tables into a temporary table
+        # (i.e: the cols containing tables of posts are appended with columns containing the post's author, which are appended with columns of the follower of these posts)
+        #                     ALL POSTS                +          AUTHORS (User)         +       FOLLOWERS (User)
+        #  =============================================================================================================
+        #  | post.id |	post.text 	   |  post.user_id   |	author.id |	author.username  |follower.id |follower.username|
+        #  =============================================================================================================
+        #  |    1    | post from susan |	   2 	     |      2 	  |      susan 	     |     1      |   	john        |
+        #  |    2    | post from mary  |	   3 	     |      3 	  |      mary 	     |     2      |     susan       |
+        #  |    3    | post from david |	   4 	     |      4 	  |      david 	     |     1      |     john        |
+        #  |    3    | post from david |	   4 	     |      4 	  |      david 	     |     3      |     mary        |
+        #  =============================================================================================================
+        # Note that post 3 is included twice because david has two followers
+        # Note that posts written that don't have followers won't be included
+        # Outer joins keep posts with no matches (no followers) on the right side of the join
+        #  |    4    | post from john  |	   1 	     |      1	  |      john 	     |     null   |     null        |
+        return (
+            sa.select(Post)
+            # Users added to the combined table from this join will be known as Author
+            .join(Post.author.of_type(Author))
+            # Users added to the combined table from this join will be known as Follower
+            .join(Author.followers.of_type(Follower), isouter=True)
+            # The resulting joins shows all posts followed by a user, we only care about those followed by the current user
+            # Here we see the usefullness of the Follower Alias, else User.id could mean the Author User or the Follower User
+            .where(sa.or_(Follower.id == self.id, Author.id == self.id))
+            # Eliminate duplicates in the final list (for example the two entries with post id 3)
+            .group_by(Post)
+            # Most recent first
+            .order_by(Post.timestamp.desc())
+        )
 
 
 class Post(db.Model):
--- app/routes.py
+++ app/routes.py
@@ -7,7 +7,7 @@ from urllib.parse import urlsplit
 
 from app import app
 from app import db
-from app.forms import EditProfileForm, LoginForm, RegistrationForm
+from app.forms import EditProfileForm, EmptyForm, LoginForm, RegistrationForm
 from app.models import User
 
 
@@ -101,4 +101,47 @@ def user(username):
         {"author": user, "body": "Test post #1"},
         {"author": user, "body": "Test post #2"},
     ]
-    return render_template("user.html", user=user, posts=posts)
+    form = EmptyForm()
+    return render_template("user.html", user=user, posts=posts, form=form)
+
+
+@app.route("/follow/<username>", methods=["POST"])
+@login_required
+def follow(username):
+    form = EmptyForm()
+    if form.validate_on_submit():
+        user = db.session.scalar(sa.select(User).where(User.username == username))
+        if user is None:
+            flash(f"User {username} not found.")
+            return redirect(url_for("index"))
+        if user == current_user:
+            flash(
+                "You're so vain. You can't follow yourself. Get your head out of your ass."
+            )
+            return redirect(url_for("user", username=username))
+        current_user.follow(user)
+        db.session.commit()
+        flash(f"You are now following {username}!")
+        return redirect(url_for("user", username=username))
+    else:
+        return redirect(url_for("index"))
+
+
+@app.route("/unfollow/<username>", methods=["POST"])
+@login_required
+def unfollow(username):
+    form = EmptyForm()
+    if form.validate_on_submit():
+        user = db.session.scalar(sa.select(User).where(User.username == username))
+        if user is None:
+            flash(f"User {username} not found.")
+            return redirect(url_for("index"))
+        if user == current_user:
+            flash("You can't unfollow yourself.")
+            return redirect(url_for("user", username=username))
+        current_user.unfollow(user)
+        db.session.commit()
+        flash(f"You are not following {username}.")
+        return redirect(url_for("user", username=username))
+    else:
+        return redirect(url_for("index"))
