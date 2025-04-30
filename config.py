12827b04139bb3c5e597f59a3dc69eccbed1ac66
enigmatic-bacon <bismuthalex@gmail.com>
2025-04-29 18:03:16 -0700

    Part 11:Bootstrap

--- config.py
+++ config.py
@@ -20,4 +20,4 @@ class Config:
     MAIL_USERNAME = os.getenv("MAIL_USERNAME")
     MAIL_PASSWORD = os.getenv("MAIL_PASSWORD")
     ADMINS = os.getenv("ADMINS")
-    POSTS_PER_PAGE = 20
+    POSTS_PER_PAGE = 5
